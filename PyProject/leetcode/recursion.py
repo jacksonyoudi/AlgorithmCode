12e8c5ce8dcad399df0cc73a2212dae095a9acdc
liangchangyou <liangchangyoujackson@gmail.com>
2020-03-24 07:25:27 +0800

    recursion

---
+++ PyProject/leetcode/recursion.py
@@ -0,0 +1,24 @@
+def recursion(level, **params):
+    # recursion terminator
+    if level > 0:
+        return
+    # process logic in current level
+
+    # drill down
+
+    # reverse the current level status if needed
+
+    """
+    recursion terminator
+    process logic in current lvel
+    drill down
+    reverse the current level status if needed
+    
+    
+    recursion terminator
+    
+    process logic in the current level
+    drill down
+    reverse the current status if needed
+    
+    """
