8fd4f35c073da3fdccb72f14e86d23bd1f6f1dea
jacksonliang <jacksonliang@zego.im>
2020-03-27 21:19:03 +0800

    bubble_sort

---
+++ PyProject/leetcode/bubble_sort.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class BubbleSort:
+    def bubble_sourt(l: List[int]) -> List[int]:
+        for i in range(len(l) - 1):
+            for j in range(len(l) - i - 1):
+                if l[j] > l[j + 1]:
+                    l[j], l[j + 1] = l[j + 1], l[j]
+        return l
+
+
+if __name__ == '__main__':
+    a = [1, 4, 3, 6, 9, 2]
+    a = BubbleSort.bubble_sourt(a)
+    print(a)
