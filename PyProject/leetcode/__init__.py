6bad0c2f0f7186d54849876ddae1e387e9990fd8
jacksonliang <jacksonliang@zego.im>
2020-05-06 14:14:57 +0800

    init

--- PyProject/leetcode/__init__.py
+++ PyProject/leetcode/__init__.py
@@ -1,5 +1,13 @@
 if __name__ == '__main__':
-    a = [1, 2, 3]
-    b = [4, 5]
-    a.extend(b)
-    print(a)
+    a = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    res = []
+    while a:
+        res += a.pop(0)
+        a = list(map(list, zip(*a)))[::-1]
+
+    print(res)
