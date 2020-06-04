61c277c25ecb543fef59ca883303b089c20a64a7
liangchangyou <liangchangyoujackson@gmail.com>
2020-06-04 08:49:04 +0800

    print binary tree

---
+++ PyProject/leetcode/print-binary-tree.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+
+# author: youdi
+# datetime: 2020/6/4 08:42
+# software: IntelliJ IDEA
+
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+def helper(nodes: List[TreeNode], result: List[List[int]]):
+    pass
+
+
+class Solution:
+    def printTree(self, root: TreeNode) -> List[List[str]]:
+        pass
