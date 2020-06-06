ecc7e36fe7fdcffcc9bd9a8ef64873c97e0ab813
jacksonyoudi <liangchangyoujackson@gmail.com>
2020-06-06 20:29:33 +0800

    map

--- PyProject/sparkproject/rdd/rdd.py
+++ PyProject/sparkproject/rdd/rdd.py
@@ -2,9 +2,21 @@
 
 from pyspark import SparkConf, SparkContext
 
+"""
+parallelize
+RDD支持的两种类型的操作： 转换 transformastions actions
+
+transformastions lazy
+
+ 
+"""
+
 if __name__ == '__main__':
-    conf = SparkConf().setAppName("rdd").setMaster("spark://127.0.0.1:17077")
+    conf = SparkConf().setAppName("rdd").setMaster("local[*]")
     sc = SparkContext(conf=conf)
     data = [1, 2, 3, 4, 5]
     disData = sc.parallelize(data, 10)
+
+    # map
+    disData = disData.map(lambda x: 2 * x)
     print(disData.collect())
