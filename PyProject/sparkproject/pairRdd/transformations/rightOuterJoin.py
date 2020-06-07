833b8535770fa371a9a1418665c0c39db7bc6967
jacksonyoudi <liangchangyoujackson@gmail.com>
2020-06-07 14:23:57 +0800

    collectasmap

--- PyProject/sparkproject/pairRdd/transformations/rightOuterJoin.py
+++ PyProject/sparkproject/pairRdd/transformations/rightOuterJoin.py
@@ -5,7 +5,8 @@ from pyspark import SparkConf, SparkContext
 if __name__ == '__main__':
     conf = SparkConf().setAppName("rdd").setMaster("local[*]")
     sc = SparkContext(conf=conf)
-    x = sc.parallelize([("a", 1)])
+    x = sc.parallelize([("a", 1), ("c", 3)])
     y = sc.parallelize([("a", 2), ("b", 2)])
     # x.leftOuterJoin(y).foreach(print)
     print(x.rightOuterJoin(y).collect())
+    print(y.fullOuterJoin(x).collect())
