b9b11e3584a3eaa5308b89afc5ced89755e832d0
jacksonyoudi <liangchangyoujackson@gmail.com>
2020-06-07 11:22:07 +0800

    left out join

---
+++ PyProject/sparkproject/pairRdd/lefOutJoin.py
@@ -0,0 +1,11 @@
+# coding: Utf-8
+
+from pyspark import SparkConf, SparkContext
+
+if __name__ == '__main__':
+    conf = SparkConf().setAppName("rdd").setMaster("local[*]")
+    sc = SparkContext(conf=conf)
+    x = sc.parallelize([("a", 1), ("b", 2)])
+    y = sc.parallelize([("a", 2)])
+    # x.leftOuterJoin(y).foreach(print)
+    print(x.leftOuterJoin(y).collect())
