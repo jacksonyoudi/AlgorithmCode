a725e6b47f08dd1aaed53019dbbb2850c12a3796
jacksonyoudi <liangchangyoujackson@gmail.com>
2020-08-13 08:42:33 +0800

    java

---
+++ src/main/java/com/logic/chap01/Demo.java
@@ -0,0 +1,15 @@
+package com.logic.chap01;
+
+
+public class Demo {
+    public static void main(String[] args) {
+        byte b = 23;
+        short s = 3333;
+        int i = 9999;
+        long l = 32323;
+        long a = 32322222L;
+        char m = 'A';
+
+        System.out.println(m);
+    }
+}
