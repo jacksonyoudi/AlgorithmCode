e5f91992475725be3d2dd61b682bb0740373617b
jacksonyoudi <liangchangyoujackson@gmail.com>
2020-08-13 14:28:01 +0800

    xunz

---
+++ src/main/java/leetcode/com/SSum.java
@@ -0,0 +1,17 @@
+package leetcode.com;
+
+/**
+ * @program: AlgorithmCode
+ * @description: one
+ * @author: changyouliang
+ * @date: 2020/07/03
+ **/
+public class SSum {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
