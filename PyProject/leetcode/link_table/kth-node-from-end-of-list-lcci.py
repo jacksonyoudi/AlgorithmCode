dca6d2a616243a81ad873cbc5ae0e338ec56eaaf
jacksonyoudi <liangchangyoujackson@gmail.com>
2020-09-09 23:25:21 +0800

    cong-wei-dao-tou-da-yin-lian-biao-lcof

---
+++ PyProject/leetcode/link_table/kth-node-from-end-of-list-lcci.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def kthToLast(self, head: ListNode, k: int) -> int:
+        if not head:
+            return None
+        slow = head
+        quick = head
+        while quick:
+            if k >= 1:
+                k -= 1
+                quick = quick.next
+            else:
+                quick = quick.next
+                slow = slow.next
+        return slow.val
