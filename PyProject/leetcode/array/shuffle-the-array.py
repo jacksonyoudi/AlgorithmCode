7af9e38e2dadc03dff59f8df7eaaa112973680c1
jacksonyoudi <liangchangyoujackson@gmail.com>
2020-09-19 18:10:28 +0800

    shuffle_the_array

---
+++ PyProject/leetcode/array/shuffle-the-array.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        result = []
+        for i in range(n):
+            result.append(nums[i])
+            result.append(nums[i + n])
+        return result
