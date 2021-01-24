5250086a905a3dc8bd8e1e5c72a4473649e1e6ec
jacksonyoudi <liangchangyoujackson@gmail.com>
2021-01-24 23:47:22 +0800

    binary-tree-inorder-traversal

--- GoProject/leetcode/tree/binary-tree-inorder-traversal.go
+++ GoProject/leetcode/tree/binary-tree-inorder-traversal.go
@@ -15,16 +15,26 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func inorderTraversalOfHelper(root *TreeNode, res []int) []int {
-	if root == nil {
-		return res
-	}
-	res = inorderTraversalOfHelper(root.Left, res)
-	res = append(res, root.Val)
-	res = inorderTraversalOfHelper(root.Right, res)
-	return res
-}
+//func inorderTraversalOfHelper(root *TreeNode, res []int) []int {
+//	if root == nil {
+//		return res
+//	}
+//	res = inorderTraversalOfHelper(root.Left, res)
+//	res = append(res, root.Val)
+//	res = inorderTraversalOfHelper(root.Right, res)
+//	return res
+//}
 
-func inorderTraversal(root *TreeNode) []int {
-	return inorderTraversalOfHelper(root, []int{})
+func inorderTraversal(root *TreeNode) (res []int) {
+	var inorder func(node *TreeNode)
+	inorder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		inorder(node.Left)
+		res = append(res, node.Val)
+		inorder(node.Right)
+	}
+	inorder(root)
+	return
 }
