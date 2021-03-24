7ab4dfaa8a622c9bab9cd5702f1ebac0d972e130
jacksonyoudi <liangchangyoujackson@gmail.com>
2021-03-24 23:44:39 +0800

    src/main/java/leetcode/com/heap/SmallestKLcci.java

--- src/main/java/leetcode/com/heap/SmallestKLcci.java
+++ src/main/java/leetcode/com/heap/SmallestKLcci.java
@@ -15,7 +15,7 @@ public class SmallestKLcci {
 
             @Override
             public int compare(Integer o1, Integer o2) {
-                return o2 - o1;
+                return o1 - o2;
             }
         });
 
