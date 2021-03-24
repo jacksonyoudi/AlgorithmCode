aaea79ce74e966e072a2f56d6f696bc73a84b178
jacksonyoudi <liangchangyoujackson@gmail.com>
2021-03-24 23:43:45 +0800

    prio

--- src/main/java/leetcode/com/heap/Kthlargestelementinanarray.java
+++ src/main/java/leetcode/com/heap/Kthlargestelementinanarray.java
@@ -1,2 +1,25 @@
-package leetcode.com.heap;public class Kthlargestelementinanarray {
+package leetcode.com.heap;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Kthlargestelementinanarray {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2-o1;
+            }
+        });
+
+        for (int num : nums) {
+            priorityQueue.offer(num);
+        }
+
+        int res = 0;
+        for (int i = 0; i < k; i++) {
+            res = (int) priorityQueue.poll();
+        }
+        return res;
+    }
 }
