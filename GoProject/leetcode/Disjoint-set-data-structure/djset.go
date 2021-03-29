5afa7ee3c45aa27d5e02ea7d88384fb2960e10c9
jacksonyoudi <liangchangyoujackson@gmail.com>
2021-03-29 23:41:42 +0800

    djset

---
+++ GoProject/leetcode/Disjoint-set-data-structure/djset.go
@@ -0,0 +1,72 @@
+package main
+
+import "os"
+
+type DjSet struct {
+	parent []int
+	size   int
+}
+
+func NewDjset(size int) *DjSet {
+	d := &DjSet{
+		parent: make([]int, size),
+		size:   size,
+	}
+	d.initialise()
+	return d
+}
+
+func (d *DjSet) initialise() {
+	for i := 0; i < d.size; i++ {
+		d.parent[i] = -1
+	}
+}
+
+func (d *DjSet) findRoot(index int) int {
+	// 默认根节点就是自己
+	root := index
+	for d.parent[root] != -1 {
+		root = d.parent[root]
+	}
+	return root
+}
+
+// 1 success 0 failure
+func (d *DjSet) unionVertices(x int, y int) int {
+	rootX := d.findRoot(x)
+	rootY := d.findRoot(y)
+
+	if rootX == rootY {
+		return 0
+	} else {
+		d.parent[rootX] = rootY
+		return 1
+	}
+
+}
+
+func main() {
+
+	edges := [5][2]int{
+		{0, 1},
+		{1, 2},
+		{1, 3},
+		{3, 4},
+		{2, 5},
+	}
+
+	djset := NewDjset(6)
+
+	for _, items := range edges {
+		x := items[0]
+		y := items[1]
+		is := djset.unionVertices(x, y)
+		if is == 0 {
+			println("cycle detected!")
+			os.Exit(1)
+		}
+	}
+
+	println("no cycle")
+
+}
