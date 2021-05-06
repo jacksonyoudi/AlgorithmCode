5398baf4cb854190a1c055477b4cac949eb9a7b5
jacksonyoudi <liangchangyoujackson@gmail.com>
2021-05-06 23:27:21 +0800

    maximum-subarray

---
+++ GoProject/leetcode/dp/maximum-subarray.go
@@ -0,0 +1,15 @@
+package dp
+
+func maxSubArray(nums []int) int {
+	max := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if  nums[i-1] > 0 {
+			nums[i] += nums[i-1]
+		}
+		if nums[i] > max {
+			max = nums[i]
+		}
+	}
+	return max
+}
+
