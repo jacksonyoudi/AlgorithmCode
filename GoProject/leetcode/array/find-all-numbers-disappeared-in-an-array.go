d4bc7c18528575937475d6241eed60fff101f045
jacksonyoudi <liangchangyoujackson@gmail.com>
2021-05-25 20:41:25 +0800

    array

--- GoProject/leetcode/array/find-all-numbers-disappeared-in-an-array.go
+++ GoProject/leetcode/array/find-all-numbers-disappeared-in-an-array.go
@@ -1,6 +1,17 @@
 package array
 
-
 func findDisappearedNumbers(nums []int) []int {
-	return nil
+	n := len(nums)
+	for _, v := range nums {
+		v = (v - 1) % n
+		nums[v] += n
+	}
+
+	ans := make([]int, 0)
+	for i, v := range nums {
+		if v <= n {
+			ans = append(ans, i+1)
+		}
+	}
+	return ans
 }
