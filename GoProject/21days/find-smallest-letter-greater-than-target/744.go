11cd31742ebec88c19921cf595cb19d247b68446
jacksonyoudi <liangchangyoujackson@gmail.com>
2022-04-03 23:35:00 +0800

    744

---
+++ GoProject/21days/find-smallest-letter-greater-than-target/744.go
@@ -0,0 +1,10 @@
+package find_smallest_letter_greater_than_target
+
+func nextGreatestLetter(letters []byte, target byte) byte {
+	for _, letter := range letters {
+		if letter > target {
+			return letter
+		}
+	}
+	return letters[0]
+}
