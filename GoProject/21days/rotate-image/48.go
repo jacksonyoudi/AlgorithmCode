ab1b4de4858e9a650a62dd0c213d8ac32b2041b8
jacksonyoudi <liangchangyoujackson@gmail.com>
2022-04-08 10:24:11 +0800

    48

--- GoProject/21days/rotate-image/48.go
+++ GoProject/21days/rotate-image/48.go
@@ -1,1 +1,15 @@
 package rotate_image
+
+func rotate(matrix [][]int) {
+	n := len(matrix)
+	if n == 0 {
+		return
+	}
+
+	for i := 0; i < n/2; i++ {
+		for j := i; j < n-1-i; j++ {
+			matrix[i][j], matrix[n-j-1][i], matrix[n-i-1][n-j-1], matrix[j][n-i-1] =
+				matrix[n-j-1][i], matrix[n-i-1][n-j-1], matrix[j][n-i-1], matrix[i][j]
+		}
+	}
+}
