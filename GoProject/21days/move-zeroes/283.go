a7155b65463a2e8501f125c815e8ca0fd6637a31
jacksonyoudi <liangchangyoujackson@gmail.com>
2022-04-09 08:18:41 +0800

    283

--- GoProject/21days/move-zeroes/283.go
+++ GoProject/21days/move-zeroes/283.go
@@ -1,1 +1,14 @@
 package move_zeroes
+
+func moveZeroes(nums []int) {
+	left, right, n := 0, 0, len(nums)
+
+	for right < n {
+		if nums[right] != 0 {
+			nums[left], nums[right] = nums[right], nums[left]
+			left++
+		}
+		right++
+	}
+
+}
