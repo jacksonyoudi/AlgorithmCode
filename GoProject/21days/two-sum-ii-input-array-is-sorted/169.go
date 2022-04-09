a963f39bd1035f50754b19a5bcbb88a78727e641
jacksonyoudi <liangchangyoujackson@gmail.com>
2022-04-09 08:19:20 +0800

    169

--- GoProject/21days/two-sum-ii-input-array-is-sorted/169.go
+++ GoProject/21days/two-sum-ii-input-array-is-sorted/169.go
@@ -1,1 +1,22 @@
 package two_sum_ii_input_array_is_sorted
+
+func twoSum(numbers []int, target int) []int {
+	n := len(numbers)
+	left, right := 0, n-1
+
+	for left < right {
+		sum := numbers[left] + numbers[right]
+		if target == sum {
+			return []int{left+1, right+1}
+		} else if target < sum {
+			right--
+			continue
+		} else if target > sum {
+			left++
+			continue
+		}
+	}
+
+	return []int{-1, -1}
+
+}
