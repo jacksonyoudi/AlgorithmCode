9763d1e5a5de3d223c84393e71209332ecf856f0
jacksonyoudi <liangchangyoujackson@gmail.com>
2022-09-22 20:36:19 +0800

    bible

---
+++ GoProject/bible/flatten-binary-tree-to-linked-list-II/114.go
@@ -0,0 +1,38 @@
+package flatten_binary_tree_to_linked_list_II
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func flatten(root *TreeNode) {
+	if root == nil {
+		return
+	}
+
+	flatten(root.Left)
+	flatten(root.Right)
+
+	left := root.Left
+	right := root.Right
+
+	root.Left = nil
+	root.Right = left
+
+	p := root
+	for p.Right != nil {
+		p = p.Right
+	}
+	p.Right = right
+
+}
